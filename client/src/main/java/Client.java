1fee0bef02f89bab389677e3b1562148593f6333
Andrey01 <Andrey.15101998@mail.ru>
2019-08-28 19:44:05 +0300

    Add client

---
+++ client/src/main/java/Client.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+    public void start(){
+        try {
+            Socket socket = new Socket(InetAddress.getLocalHost(), 9876);
+            BufferedReader sockIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter sockOut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            new Thread(() -> {
+                transfer(in, sockOut);
+             }).start();
+
+            new Thread(() -> {
+                transfer(sockIn, out);
+            }).start();
+
+        } catch (IOException e) {
+            e.printStackTrace();//TODO log
+        }
+    }
+
+    private void transfer(BufferedReader from, BufferedWriter to) {
+        try {
+            to.write(from.readLine());
+            to.flush();
+        } catch (IOException e) {
+            e.printStackTrace();//TODO log
+        }
+    }
+
+}
