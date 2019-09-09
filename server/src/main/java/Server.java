589100dbe2bc5833dd359df1904e533ce034c4b8
Andrey01 <Andrey.15101998@mail.ru>
2019-09-09 11:45:25 +0300

    All done. It should work

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -6,21 +6,23 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.*;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class Server {
+    private final static Logger log = LogManager.getLogger(Server.class);
+
     private Map<SocketChannel, UserSession> users = new HashMap<>();
     private Queue<UserSession> freeAgents = new ArrayDeque<>();
 
     public void start() {
-        //new Thread(() -> {
         try {
             Selector selector = Selector.open();
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             InetSocketAddress address = new InetSocketAddress("localhost", 9876);
             serverSocketChannel.bind(address);
             serverSocketChannel.configureBlocking(false);
-            serverSocketChannel.register(selector, serverSocketChannel.validOps());
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
             while (selector.select() > -1) {
                 Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
@@ -31,8 +33,6 @@ public class Server {
                     if (key.isValid()) {
                         if (key.isAcceptable())
                             accept(key);
-                        else if (key.isConnectable())
-                            connect(key);
                         else if (key.isReadable())
                             read(key);
                     }
@@ -40,9 +40,8 @@ public class Server {
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("Сервер наебнулся", e);
         }
-        //}).start();
     }
 
     private void accept(SelectionKey key) throws IOException {
@@ -60,19 +59,49 @@ public class Server {
 
     private void read(SelectionKey key) throws IOException {
         SocketChannel channel = ((SocketChannel) key.channel());
-        String str = readChannel(channel);
+        String str = readInput(channel);
+        if (str == null)
+            key.cancel();
+        else processString(channel, str);
+    }
+
+    private String readInput(SocketChannel channel) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int length = channel.read(buffer);
+
+        if (length == -1){
+            UserSession session = users.get(channel);
+            if (session.getType())
+                log.info("Client" + session.getUser().getNick() + " disconnected");
+            else
+                log.info("Agent" + session.getUser().getNick() + " disconnected");
+            UserSession pair = session.getPair();
+            if (pair != null) {
+                pair.getChannel().write(ByteBuffer.wrap("System massage: user suddenly disconnected".getBytes()));
+                pair.setPair(null);
+                if (!pair.getType())
+                    freeAgents.add(pair);
+            }
+            users.remove(channel);
+            channel.close();
+            return null;
+        }
+
+        byte[] data = new byte[length];
+        System.arraycopy(buffer.array(), 0, data, 0, length);
+
+        return new String(data);
+    }
+
+    private void processString(SocketChannel channel, String str) throws IOException{
         if (str.startsWith("/register ")) {
             if (str.indexOf("agent ") == 10)
                 registerUser(channel, false, str.substring(16));
             else if (str.indexOf("client ") == 10)
                 registerUser(channel, true, str.substring(17));
-            else {
-                //TODO ask to repeat
-            }
+            else channel.write(ByteBuffer.wrap("System massage: command not recognized. Reissue the command".getBytes()));
         } else if (str.equals("/leave"))
-            exit(channel, true);
-        else if (str.equals("/exit"))
-            exit(channel, false);
+            leave(channel);
         else
             redirect(channel, str);
     }
@@ -81,44 +110,33 @@ public class Server {
         UserSession session = users.get(channel);
         session.setType(type);
         session.getUser().setNick(nick);
-        if (!type) freeAgents.add(session);
+        if (!type) {
+            freeAgents.add(session);
+            log.info("Registered user " + nick);
+        } else log.info("Registered agent " + nick);
     }
 
     private void redirect(SocketChannel channel, String str) throws IOException {
         UserSession session = users.get(channel);
         if (session.getPair() == null) {
             if (freeAgents.isEmpty()) {
-                //TODO ask to wait
+                channel.write(ByteBuffer.wrap("System massage: no available agents. Please wait and try again".getBytes()));
             } else {
                 session.setPair(freeAgents.poll());
                 session.getPair().setPair(session);
+                log.info("User " + session.getUser().getNick() + " join chat with agent " + session.getPair().getUser().getNick());
             }
         }
         session.getPair().getChannel().write(ByteBuffer.wrap(str.getBytes()));
     }
 
-    private void exit(SocketChannel channel, boolean leave) {
+    private void leave(SocketChannel channel) {
         UserSession session = users.get(channel);
-        if (session.getType()) {
-            if (leave) return;
-            else freeAgents.add(session.getPair());
-        } else
-            freeAgents.add(session);
-
+        if (session.getPair() == null) return;
+        if (session.getType())
+            freeAgents.add(session.getPair());
+        log.info("User " + session.getUser().getNick() + " left chat with agent " + session.getPair().getUser().getNick());
         session.getPair().setPair(null);
         session.setPair(null);
-    }
-
-    private String readChannel(SocketChannel channel) throws IOException {
-        ByteBuffer bb = ByteBuffer.allocate(1000);
-        channel.read(bb);
-        bb.flip();
-        String str = new String(bb.array());
-        bb.clear();
-        return str;
-    }
-
-    private void connect(SelectionKey key) {
-
     }
 }
